76dec1cdf6b3115855c72c01782d43b806d272a2
Agustinus Kristiadi <agustinus@kristia.de>
2024-05-02 20:55:29 -0400

    Progress on scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -20,9 +20,24 @@ impl Scanner {
     }
 
     pub fn scan_tokens(&mut self) {
-        while self.current >= self.source.len() {
+        while !self.is_at_end() {
             self.start = self.current;
-            self.scan_token();
+
+            let token_type: Option<TokenType> = self.scan_single_token();
+
+            if token_type.is_none() {
+                continue;
+            }
+
+            let token_type: TokenType = token_type.unwrap();
+            let literal: Option<String> = match token_type {
+                TokenType::String => self.get_string().clone(),
+                _ => None,
+            };
+            let text: &str = &self.source[self.start..self.current];
+
+            self.tokens
+                .push(Token::new(token_type, text.to_string(), literal, self.line));
         }
 
         self.tokens.push(Token::new(
@@ -33,25 +48,60 @@ impl Scanner {
         ));
     }
 
-    fn scan_token(&mut self) {
-        let c: char = self.advance();
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
 
-        self.add_token(
-            match c {
-                '(' => TokenType::LeftParen,
-                ')' => TokenType::RightParen,
-                '{' => TokenType::LeftBrace,
-                '}' => TokenType::RightBrace,
-                ',' => TokenType::Comma,
-                '.' => TokenType::Dot,
-                '-' => TokenType::Minus,
-                '+' => TokenType::Plus,
-                ';' => TokenType::Semicolon,
-                '*' => TokenType::Star,
-                _ => unreachable!(),
+    fn scan_single_token(&mut self) -> Option<TokenType> {
+        let next_char: char = self.advance();
+
+        let token_type: Option<TokenType> = match next_char {
+            '(' => Some(TokenType::LeftParen),
+            ')' => Some(TokenType::RightParen),
+            '{' => Some(TokenType::LeftBrace),
+            '}' => Some(TokenType::RightBrace),
+            ',' => Some(TokenType::Comma),
+            '.' => Some(TokenType::Dot),
+            '-' => Some(TokenType::Minus),
+            '+' => Some(TokenType::Plus),
+            ';' => Some(TokenType::Semicolon),
+            '*' => Some(TokenType::Star),
+            '!' => match self.matches('=') {
+                true => Some(TokenType::BangEqual),
+                false => Some(TokenType::Bang),
             },
-            Option::None,
-        )
+            '=' => match self.matches('=') {
+                true => Some(TokenType::EqualEqual),
+                false => Some(TokenType::Equal),
+            },
+            '>' => match self.matches('=') {
+                true => Some(TokenType::GreaterEqual),
+                false => Some(TokenType::Greater),
+            },
+            '<' => match self.matches('=') {
+                true => Some(TokenType::LessEqual),
+                false => Some(TokenType::Less),
+            },
+            '/' => match self.matches('/') {
+                // Read the whole comment line
+                true => {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                    None
+                }
+                false => Some(TokenType::Slash),
+            },
+            ' ' | '\r' | '\t' => None,
+            '\n' => {
+                self.line += 1;
+                None
+            }
+            '"' => Some(TokenType::String),
+            _ => unreachable!(),
+        };
+
+        token_type
     }
 
     fn advance(&mut self) -> char {
@@ -60,9 +110,42 @@ impl Scanner {
         next_char
     }
 
-    fn add_token(&mut self, token_type: TokenType, literal: Option<String>) {
-        let text: &str = &self.source[self.start..self.current];
-        self.tokens
-            .push(Token::new(token_type, text.to_string(), literal, self.line));
+    fn matches(&mut self, expected: char) -> bool {
+        if self.current >= self.source.len() {
+            return false;
+        }
+        if self.source.chars().nth(self.current).unwrap() != expected {
+            return false;
+        }
+
+        self.current += 1;
+        true
+    }
+
+    fn peek(&self) -> char {
+        match self.is_at_end() {
+            true => '\0',
+            false => self.source.chars().nth(self.current).unwrap(),
+        }
+    }
+
+    fn get_string(&mut self) -> Option<String> {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            println!("Unterminated string.");
+            return None;
+        }
+
+        self.advance(); // Move cursor to the closing "
+
+        // Trim the quotes, get the string itself
+        let value: &str = &self.source[(self.start + 1)..(self.current - 1)];
+        Some(value.to_string())
     }
 }
