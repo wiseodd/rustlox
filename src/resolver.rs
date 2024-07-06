76a766e1d1e6a02809cd9d92c18f7e7ff1c0e94b
Agustinus Kristiadi <agustinus@kristia.de>
2024-07-06 09:42:07 -0400

    Add resolver for variable expressions

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,4 +1,6 @@
-use crate::{expr::Expr, interpreter::Interpreter, stmt::Stmt, token::Token};
+use crate::{
+    error::LoxError, expr::Expr, interpreter::Interpreter, lox::Lox, stmt::Stmt, token::Token,
+};
 use std::collections::HashMap;
 
 // #[derive(Debug, Default, Clone)]
@@ -33,7 +35,7 @@ impl Resolver {
             Stmt::Var { name, initializer } => {
                 self.declare(name.clone());
                 if let Some(init) = initializer {
-                    self.resolve_expr(init.clone());
+                    self.resolve_expr(&init);
                 }
                 self.define(name.clone());
             }
@@ -41,8 +43,29 @@ impl Resolver {
         };
     }
 
-    pub fn resolve_expr(&mut self, expr: Expr) {
-        todo!();
+    pub fn resolve_expr(&mut self, expr: &Expr) {
+        match expr {
+            Expr::Variable { name } => {
+                if let Some(scope) = self.scopes.last() {
+                    if let Some(resolved) = scope.get(&name.lexeme) {
+                        if !resolved {
+                            Lox::parse_error(
+                                name,
+                                "Can't read local variable in its own initializer.",
+                            );
+                        }
+                    }
+                }
+                self.resolve_local(expr, name.clone());
+            }
+            Expr::Assign { name, value } => {
+                // Recursively resolve the value of this assignment since it can
+                // contain references to other variables (e.g. `var x = (a == b)`)
+                self.resolve_expr(value);
+                self.resolve_local(expr, name.clone());
+            }
+            _ => unreachable!(),
+        };
     }
 
     fn begin_scope(&mut self) {
@@ -74,6 +97,19 @@ impl Resolver {
         // Mark the declared varible as resolved
         if let Some(scope) = self.scopes.last_mut() {
             scope.insert(name.lexeme, true);
+        }
+    }
+
+    fn resolve_local(&self, expr: &Expr, name: Token) {
+        // Starting from the innermost scope (top of the stack), we check for `name`.
+        // Then resolve it under the correct scope.
+        for i in (self.scopes.len() - 1)..0 {
+            if let Some(scope) = self.scopes.get(i) {
+                if scope.contains_key(&name.lexeme) {
+                    // self.interpreter.resolve(expr, scopes.len() - 1 - i);
+                    return;
+                }
+            }
         }
     }
 }
